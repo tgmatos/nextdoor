cca4f0e8e69e75241590bdc8ee4b3d9cd2115825
Tiago Matos <tiagomatos3216@gmail.com>
2025-05-27 20:08:54 -0300

    Add CORS

---
+++ backend/lib/next_door_web/cors.ex
@@ -0,0 +1,14 @@
+defmodule NextDoorWeb.CORS do
+  use Corsica.Router,
+    origins: ["http://localhost:5173"],
+    allow_credentials: true,
+    allow_methods: :all,
+    allow_headers: :all,
+    max_age: 7200
+
+  resource("/api/store/*",
+    origins: ["http://localhost:5173"],
+    allow_methods: :all,
+    allow_headers: :all
+  )
+end
