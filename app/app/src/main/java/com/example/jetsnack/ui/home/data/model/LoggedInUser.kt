e1b9aeda2e477861678b57dcb56b538bf1b5e8d9
Tiago Matos <tiagomatos3216@gmail.com>
2026-08-06 19:36:43 -0300

    Remove old app

--- app/app/src/main/java/com/example/jetsnack/ui/home/data/model/LoggedInUser.kt
+++
@@ -1,9 +0,0 @@
-package com.example.jetsnack.ui.home.data.model
-
-/**
- * Data class that captures user information for logged in users retrieved from LoginRepository
- */
-data class LoggedInUser(
-    val userId: String,
-    val displayName: String
-)
